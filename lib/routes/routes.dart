862bbcfc3e3259ccd24669b50f717af221e4fe11
naveen <naveenramachandru77@gmail.com>
2021-12-20 22:33:27 +0530

    ui some updated

---
+++ lib/routes/routes.dart
@@ -0,0 +1,17 @@
+import 'package:buzz_ai/screens/slpashscreen/splashscrenn.dart';
+import 'package:get/get.dart';
+
+abstract class Routes {
+  static const INITIAL = '/';
+}
+
+abstract class AppPages {
+  static final pages = [
+    GetPage(
+      name: Routes.INITIAL,
+      page: () => SplashScreen(),
+    ),
+
+    // GetPage(name: Routes.APRESENTACAO, page:()=> ApresentacaoPage()),
+  ];
+}
---
+++ lib/services/locale/bindings.dart
@@ -0,0 +1,9 @@
+import 'package:get/get_instance/src/bindings_interface.dart';
+import 'package:get/instance_manager.dart';
+
+class ControllerBindings extends Bindings {
+  @override
+  void dependencies() {
+    // TODO: implement dependencies
+  }
+}
