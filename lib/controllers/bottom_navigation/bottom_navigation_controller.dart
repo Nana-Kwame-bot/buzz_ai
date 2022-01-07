d7625b5b71e24df15f95a21a2cdf90d43bbd7f9e
Piyush Singhal <piyushlimited@gmail.com>
2022-01-07 21:03:32 +0530

    Set Navigation for Report Screen

--- lib/controllers/bottom_navigation/bottom_navigation_controller.dart
+++ lib/controllers/bottom_navigation/bottom_navigation_controller.dart
@@ -1,4 +1,5 @@
 import 'package:buzz_ai/controllers/authentication/authentication_controller.dart';
+import 'package:buzz_ai/screens/accidentreport_screen/accidentreport_screen.dart';
 import 'package:buzz_ai/screens/home/home_screen.dart';
 import 'package:buzz_ai/screens/profile_screen/profile_screen.dart';
 import 'package:fancy_bottom_navigation/fancy_bottom_navigation.dart';
@@ -30,9 +31,7 @@ class BottomNavigationController extends ChangeNotifier {
     Container(
       color: Colors.greenAccent,
     ),
-    Container(
-      color: Colors.redAccent,
-    ),
+    const AccidentReportScreen(),
     const ProfileScreen(),
   ];
 }
