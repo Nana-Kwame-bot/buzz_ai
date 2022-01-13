aac5de151220dacd9ed097af00b4feb02b41f0a7
hiruthicShaSS <predatorsah2002@gmail.com>
2022-01-13 20:07:44 +0530

    Resetting the form after successfull reporting

--- lib/screens/accidentreport_screen/accidentreport_screen.dart
+++ lib/screens/accidentreport_screen/accidentreport_screen.dart
@@ -88,16 +88,19 @@ class _AccidentReportScreenState extends State<AccidentReportScreen> {
 
     String reportID =
         "${Random.secure().nextInt(999).toString()}_${DateTime.now().millisecondsSinceEpoch}";
-    await FirebaseFirestore.instance
-        .collection('accidentReports')
-        .doc(_uid)
-        .collection("reports")
-        .doc(reportID)
-        .set(accidentReport.toJSON());
+    // await FirebaseFirestore.instance
+    //     .collection('accidentReports')
+    //     .doc(_uid)
+    //     .collection("reports")
+    //     .doc(reportID)
+    //     .set(accidentReport.toJSON());
     setState(() {
       _uploading = false;
     });
 
+    carPlateNumber.text = "";
+    numberOfPeopleInjured.text = "0";
+    imageFile = null;
     showDialog(
       context: context,
       builder: (context) => AlertDialog(
