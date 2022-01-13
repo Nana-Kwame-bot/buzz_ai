45a0c5187cb655da4761fb0140686c001d8d23ee
hiruthicShaSS <predatorsah2002@gmail.com>
2022-01-13 16:27:45 +0530

    Report accident page fixed
    Rate limiting should be implemented to avoid spam

---
+++ lib/models/report_accident/accident_report.dart
@@ -0,0 +1,57 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:geolocator/geolocator.dart';
+
+class AccidentReport {
+  late DateTime createdAt;
+  late String imageURL;
+  late String carPlateNumber;
+  late int peopleInjured;
+  late List<double> coordinates;
+  late Map location;
+  String? reportID;
+
+  AccidentReport({
+    required this.createdAt,
+    required this.imageURL,
+    required this.carPlateNumber,
+    required this.peopleInjured,
+    required this.coordinates,
+    required this.location,
+  });
+
+  Map<String, dynamic> toJSON() => {
+        "createdAt": createdAt,
+        "imageURL": imageURL,
+        "carPlateNumber": carPlateNumber,
+        "peopleInjured": peopleInjured,
+        "coordinates": coordinates,
+        "location": location,
+      };
+
+  AccidentReport fromJson(Map<String, dynamic> json) {
+    return AccidentReport(
+      createdAt: DateTime.parse(json["createdAt"]),
+      imageURL: json["imageURL"],
+      carPlateNumber: json["carPlateNumber"],
+      peopleInjured: json["peopleInjured"],
+      coordinates: json["coordinates"],
+      location: json["location"],
+    );
+  }
+
+  AccidentReport.fromSnapshot(
+      QueryDocumentSnapshot<Map<String, dynamic>> snapshot) {
+    createdAt = snapshot.data()["createdAt"].toDate();
+    imageURL = snapshot.data()["imageURL"];
+    carPlateNumber = snapshot.data()["carPlateNumber"];
+    peopleInjured = snapshot.data()["peopleInjured"];
+    coordinates = List<double>.from(snapshot.data()["coordinates"]);
+    location = snapshot.data()["location"];
+    reportID = snapshot.id;
+  }
+
+  @override
+  String toString() {
+    return 'AccidentReport(createdAt: $createdAt, imageURL: $imageURL, carPlateNumber: $carPlateNumber, peopleInjured: $peopleInjured, coordinates: $coordinates, location: $location, reportID: $reportID)';
+  }
+}
--- lib/screens/accidentreport_screen/accidentreport_screen.dart
+++ lib/screens/accidentreport_screen/accidentreport_screen.dart
@@ -1,8 +1,14 @@
 import 'dart:io';
+import 'dart:math';
+import 'package:buzz_ai/models/report_accident/accident_report.dart';
 import 'package:buzz_ai/models/report_accident/submit_accident_report.dart';
+import 'package:buzz_ai/services/get_location.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:geocoding/geocoding.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:provider/provider.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
@@ -18,19 +24,19 @@ class AccidentReportScreen extends StatefulWidget {
 
 class _AccidentReportScreenState extends State<AccidentReportScreen> {
   final FirebaseAuth auth = FirebaseAuth.instance;
+  GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+  final picker = ImagePicker();
+  File? imageFile;
+  late TextEditingController carPlateNumber;
+  late TextEditingController numberOfPeopleInjured;
 
   @override
   void initState() {
-    carPlateNumber = TextEditingController(text: 'Car Number Plate');
-    numberOfPeopleInjured = TextEditingController(text: '0');
+    carPlateNumber = TextEditingController();
+    numberOfPeopleInjured = TextEditingController(text: '1');
     super.initState();
   }
-
-  final picker = ImagePicker();
-  late File imageFile;
-  dynamic carNumberPlate, peopleInjured;
-  late TextEditingController? carPlateNumber;
-  late TextEditingController? numberOfPeopleInjured;
 
   Future<void> pickImage() async {
     // dynamic picture = await picker.pickImage(source: ImageSource.camera);
@@ -42,34 +48,68 @@ class _AccidentReportScreenState extends State<AccidentReportScreen> {
     final pickedImage = await picker.pickImage(
       source: ImageSource.camera,
     );
-    final pickedImageFile = File(pickedImage!.path);
+
+    if (pickedImage == null) return;
+
+    final pickedImageFile = File(pickedImage.path);
     setState(() {
       imageFile = pickedImageFile;
     });
   }
 
   Future<void> uploadAccidentReport() async {
+    setState(() {
+      _uploading = true;
+    });
     final User? user = auth.currentUser;
     final _uid = user?.uid;
 
     final ref = FirebaseStorage.instance
         .ref(_uid!)
         // .child(_uid)
-        .child('accidentImage.jpg');
-    await ref.putFile(imageFile);
+        .child('accidentImage-${DateTime.now().millisecondsSinceEpoch}.jpg');
+    await ref.putFile(imageFile!);
     var url = await ref.getDownloadURL();
 
+    Map locationData = await getLocation();
+    Position position = locationData["position"];
+    Placemark location = locationData["placemark"];
+
+    AccidentReport accidentReport = AccidentReport(
+      createdAt: DateTime.now(),
+      imageURL: url,
+      carPlateNumber: carPlateNumber.text,
+      peopleInjured: int.parse(numberOfPeopleInjured.text),
+      coordinates: [position.latitude, position.longitude],
+      location: location.toJson(),
+    );
+
+    FocusScope.of(context).unfocus();
+
+    String reportID =
+        "${Random.secure().nextInt(999).toString()}_${DateTime.now().millisecondsSinceEpoch}";
     await FirebaseFirestore.instance
-        .collection('uploadAccidentReport')
-        .doc(_uid + "." + "${Timestamp.now()}")
-        .set({
-      'carNumberPlate': carPlateNumber,
-      'peopleInjured': numberOfPeopleInjured,
-      'AccidentImage': url,
-      // 'userId': _uid,
-      'createdAt': Timestamp.now(),
+        .collection('accidentReports')
+        .doc(_uid)
+        .collection("reports")
+        .doc(reportID)
+        .set(accidentReport.toJSON());
+    setState(() {
+      _uploading = false;
     });
-    Navigator.canPop(context) ? Navigator.pop(context) : null;
+
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        title: const Text("Success"),
+        content: const Text("Accident reported successfuly!"),
+        actions: [
+          TextButton(
+              onPressed: () => Navigator.of(context).pop(),
+              child: const Text("OK")),
+        ],
+      ),
+    );
   }
 
   showAlertDialog(BuildContext context) {
@@ -99,6 +139,8 @@ class _AccidentReportScreenState extends State<AccidentReportScreen> {
     );
   }
 
+  bool _uploading = false;
+
   @override
   Widget build(BuildContext context) {
     final validationService = Provider.of<SubmitAccidentReport>(context);
@@ -121,6 +163,13 @@ class _AccidentReportScreenState extends State<AccidentReportScreen> {
             ),
           ),
           centerTitle: true,
+          bottom: PreferredSize(
+            preferredSize: const Size.fromHeight(0.4),
+            child: Visibility(
+              visible: _uploading,
+              child: const LinearProgressIndicator(),
+            ),
+          ),
         ),
         backgroundColor: Colors.white,
         body: ListView(
@@ -178,143 +227,154 @@ class _AccidentReportScreenState extends State<AccidentReportScreen> {
                       color: Colors.white,
                       child: Padding(
                         padding: const EdgeInsets.fromLTRB(10, 30, 20, 0),
-                        child: Column(
-                          children: [
-                            const SizedBox(
-                              height: 25,
-                            ),
-                            Row(children: const [
-                              Expanded(
-                                  child: Divider(
-                                color: Colors.black45,
-                                indent: 0,
-                                endIndent: 5,
-                              )),
-                              Text(
-                                "Car Number Plate",
-                                style: TextStyle(
+                        child: Form(
+                          key: _formKey,
+                          child: Column(
+                            children: [
+                              const SizedBox(
+                                height: 25,
+                              ),
+                              Row(children: const [
+                                Expanded(
+                                    child: Divider(
                                   color: Colors.black45,
-                                  fontSize: 14,
+                                  indent: 0,
+                                  endIndent: 5,
+                                )),
+                                Text(
+                                  "Car Number Plate",
+                                  style: TextStyle(
+                                    color: Colors.black45,
+                                    fontSize: 14,
+                                  ),
                                 ),
-                              ),
-                              Expanded(
-                                  child: Divider(
-                                color: Colors.black45,
-                                indent: 5,
-                                endIndent: 0,
-                              )),
-                            ]),
-                            const SizedBox(
-                              height: 10,
-                            ),
-                            TextFormField(
-                              textAlign: TextAlign.center,
-                              style: const TextStyle(fontSize: 14, height: 1),
-                              decoration: const InputDecoration(
-                                focusedBorder: OutlineInputBorder(
-                                  borderSide: BorderSide(
-                                      color: Colors.black45, width: 1.0),
-                                ),
-                                enabledBorder: OutlineInputBorder(
-                                  borderSide: BorderSide(
-                                      color: Colors.black45, width: 1.0),
-                                ),
-                                hintText: 'Car Number Plate',
-                              ),
-                              controller: carPlateNumber,
-                              validator: (value) =>
-                                  value!.isEmpty ? "Enter Email" : null,
-                              onChanged: (dynamic value) async {
-                                carNumberPlate = await validationService
-                                    .changeCarNumberPlate(value);
-                                setState(() {
-                                  carPlateNumber = value;
-                                });
-                              },
-                            ),
-                            const SizedBox(
-                              height: 5,
-                            ),
-                            Row(children: const [
-                              Expanded(
-                                  child: Divider(
-                                color: Colors.black45,
-                                indent: 0,
-                                endIndent: 5,
-                              )),
-                              Text(
-                                "How many people were injured",
-                                style: TextStyle(
+                                Expanded(
+                                    child: Divider(
                                   color: Colors.black45,
-                                  fontSize: 14,
-                                ),
+                                  indent: 5,
+                                  endIndent: 0,
+                                )),
+                              ]),
+                              const SizedBox(
+                                height: 10,
                               ),
-                              Expanded(
-                                  child: Divider(
-                                color: Colors.black45,
-                                indent: 5,
-                                endIndent: 0,
-                              )),
-                            ]),
-                            const SizedBox(
-                              height: 10,
-                            ),
-                            TextFormField(
-                              textAlign: TextAlign.center,
-                              keyboardType: TextInputType.number,
-                              style: const TextStyle(fontSize: 14, height: 1),
-                              decoration: const InputDecoration(
-                                focusedBorder: OutlineInputBorder(
-                                  borderSide: BorderSide(
-                                      color: Colors.black45, width: 1.0),
+                              TextFormField(
+                                textAlign: TextAlign.center,
+                                style: const TextStyle(fontSize: 14, height: 1),
+                                decoration: const InputDecoration(
+                                  focusedBorder: OutlineInputBorder(
+                                    borderSide: BorderSide(
+                                        color: Colors.black45, width: 1.0),
+                                  ),
+                                  enabledBorder: OutlineInputBorder(
+                                    borderSide: BorderSide(
+                                        color: Colors.black45, width: 1.0),
+                                  ),
+                                  hintText: 'Car Number Plate',
                                 ),
-                                enabledBorder: OutlineInputBorder(
-                                  borderSide: BorderSide(
-                                      color: Colors.black45, width: 1.0),
-                                ),
-                                hintText: ' How many people are injured',
-                              ),
-                              controller: numberOfPeopleInjured,
-                              onChanged: (dynamic value) {
-                                peopleInjured = validationService
-                                    .changeNumberOfPeopleInjured(value);
-                                setState(() {
-                                  numberOfPeopleInjured = value;
-                                });
-                              },
-                            ),
-                            const SizedBox(
-                              height: 20,
-                            ),
-                            Container(
-                              width: double.infinity,
-                              color: const Color.fromRGBO(82, 71, 197, 1),
-                              child: TextButton(
-                                onPressed: () async {
-                                  if (imageFile.path.isNotEmpty &&
-                                      carNumberPlate
-                                          .toString()
-                                          .trim()
-                                          .isNotEmpty) {
-                                    await uploadAccidentReport();
-                                  } else {
-                                    if (imageFile.path.isEmpty) {
-                                      showAlertDialog(context);
-                                    } else if (carNumberPlate
-                                        .toString()
-                                        .trim()
-                                        .isEmpty) {
-                                      showAlertDialog(context);
-                                    }
-                                  }
+                                controller: carPlateNumber,
+                                validator: (value) {
+                                  if (value!.isEmpty)
+                                    return "Enter Car Plate Number";
+                                  if (value.length < 3)
+                                    return "Enter the valid car plate number!";
                                 },
-                                child: const Text(
-                                  "Submit",
-                                  style: TextStyle(color: Colors.white),
-                                ),
                               ),
-                            )
-                          ],
+                              const SizedBox(
+                                height: 5,
+                              ),
+                              Row(children: const [
+                                Expanded(
+                                    child: Divider(
+                                  color: Colors.black45,
+                                  indent: 0,
+                                  endIndent: 5,
+                                )),
+                                Text(
+                                  "How many people were injured",
+                                  style: TextStyle(
+                                    color: Colors.black45,
+                                    fontSize: 14,
+                                  ),
+                                ),
+                                Expanded(
+                                    child: Divider(
+                                  color: Colors.black45,
+                                  indent: 5,
+                                  endIndent: 0,
+                                )),
+                              ]),
+                              const SizedBox(
+                                height: 10,
+                              ),
+                              TextFormField(
+                                textAlign: TextAlign.center,
+                                keyboardType: TextInputType.number,
+                                inputFormatters: [
+                                  FilteringTextInputFormatter.digitsOnly
+                                ],
+                                style: const TextStyle(fontSize: 14, height: 1),
+                                decoration: const InputDecoration(
+                                  focusedBorder: OutlineInputBorder(
+                                    borderSide: BorderSide(
+                                        color: Colors.black45, width: 1.0),
+                                  ),
+                                  enabledBorder: OutlineInputBorder(
+                                    borderSide: BorderSide(
+                                        color: Colors.black45, width: 1.0),
+                                  ),
+                                  hintText: ' How many people are injured',
+                                ),
+                                controller: numberOfPeopleInjured,
+                                validator: (value) {
+                                  if (int.parse(value ?? "0") == 0)
+                                    return "Injured people must be atleast 1";
+                                },
+                                onChanged: (dynamic value) {
+                                  validationService
+                                      .changeNumberOfPeopleInjured(value);
+                                },
+                              ),
+                              const SizedBox(
+                                height: 20,
+                              ),
+                              Container(
+                                width: double.infinity,
+                                color: const Color.fromRGBO(82, 71, 197, 1),
+                                child: TextButton(
+                                  onPressed: () async {
+                                    if (_formKey.currentState!.validate()) {
+                                      if (imageFile == null) {
+                                        showAlertDialog(context);
+                                        return;
+                                      }
+
+                                      if (imageFile!.path.isNotEmpty &&
+                                          carPlateNumber.text
+                                              .toString()
+                                              .trim()
+                                              .isNotEmpty) {
+                                        await uploadAccidentReport();
+                                      } else {
+                                        if (imageFile!.path.isEmpty) {
+                                          showAlertDialog(context);
+                                        } else if (carPlateNumber.text
+                                            .toString()
+                                            .trim()
+                                            .isEmpty) {
+                                          showAlertDialog(context);
+                                        }
+                                      }
+                                    }
+                                  },
+                                  child: const Text(
+                                    "Submit",
+                                    style: TextStyle(color: Colors.white),
+                                  ),
+                                ),
+                              )
+                            ],
+                          ),
                         ),
                       ),
                     ),
