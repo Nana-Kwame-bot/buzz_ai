4521492e1162f0d07a13f31935c1925ac4422045
hiruthicShaSS <predatorsha2002@gmail.com>
2022-01-31 18:54:05 +0530

    Pending file upload fixed, optimized and can run in background

--- lib/activity_recognition.dart
+++ lib/activity_recognition.dart
@@ -112,48 +112,6 @@ class ActivityRecognitionApp with ChangeNotifier {
     else {
       startTracking();
     }
-
-    Timer.periodic(const Duration(seconds: 3), (timer) async {
-      DateTime now = DateTime.now();
-
-      if (now.hour == 2 && now.minute >= 30) {
-        Connectivity()
-            .onConnectivityChanged
-            .listen((ConnectivityResult result) async {
-          if ([ConnectivityResult.mobile, ConnectivityResult.wifi]
-              .contains(result)) {
-            String path = (await getApplicationDocumentsDirectory()).path;
-
-            if (prefs.getBool("sensorUploadPending") ?? false) {
-              List<String> allPendingUploads =
-                  prefs.getStringList("sensorDataPendingUploads") ?? [];
-
-              for (var pendingFile in allPendingUploads) {
-                File file = File("$path/$pendingFile");
-                await uploadSensorData(file);
-              }
-              prefs.setStringList("sensorDataPendingUploads", []);
-            }
-            await uploadSensorData();
-          } else {
-            // Queue all pending files
-            DateTime today = DateTime.now();
-            await prefs.setBool("sensorUploadPending", true);
-            List<String> allPendingUploads = [
-              "sensordata-${today.day}-${today.month}-${today.year}.csv",
-            ];
-
-            allPendingUploads
-                .addAll(prefs.getStringList("sensorDataPendingUploads") ?? []);
-
-            await prefs.setStringList(
-              "sensorDataPendingUploads",
-              allPendingUploads,
-            );
-          }
-        });
-      }
-    });
   }
 
   Future<void> _recordAudio() async {
@@ -268,49 +226,6 @@ class ActivityRecognitionApp with ChangeNotifier {
     _lastWritten = DateTime.now();
   }
 
-  Future<String> readSensorData([File? infile]) async {
-    Directory dir = await getApplicationDocumentsDirectory();
-    DateTime today = DateTime.now();
-    File file = infile ??
-        File(
-            "${dir.path}/sensordata-${today.day}-${today.month}-${today.year}.csv");
-
-    return await file.readAsString();
-  }
-
-  bool _sensorDataUploaded = false;
-  Future<void> uploadSensorData([File? file]) async {
-    if (_sensorDataUploaded) return;
-
-    String uid = FirebaseAuth.instance.currentUser!.uid;
-
-    String data = await readSensorData();
-    DateTime today = DateTime.now();
-
-    TaskSnapshot sensorDataUploadTask = await FirebaseStorage.instance
-        .ref(uid)
-        .child("sensor_data")
-        .child("${today.day}-${today.month}-${today.year}.csv")
-        .putString(data, format: PutStringFormat.raw);
-
-    await FirebaseFirestore.instance
-        .collection("userDatabase")
-        .doc(uid)
-        .update({
-      "sensorData": FieldValue.arrayUnion([
-        {
-          "timestamp": DateTime.now(),
-          "filePath": await sensorDataUploadTask.ref.getDownloadURL()
-        }
-      ])
-    });
-    _sensorDataUploaded = true;
-    Directory dir = await getApplicationDocumentsDirectory();
-    await File(
-            "${dir.path}/sensordata-${today.day}-${today.month}-${today.year}.csv")
-        .delete();
-  }
-
   _updateActivityNotification(ActivityEvent currentActivityEvent) {
     AwesomeNotifications().isNotificationAllowed().then((isAllowed) {
       if (!isAllowed) {
@@ -334,7 +249,6 @@ class ActivityRecognitionApp with ChangeNotifier {
           title: title,
           body: body,
         ),
-        
         actionButtons: [
           NotificationActionButton(
             key: "open",
--- lib/main.dart
+++ lib/main.dart
@@ -163,7 +163,7 @@ class _MyAppState extends State<MyApp> with WidgetsBindingObserver {
 
   @override
   Widget build(BuildContext context) {
-    return _deviceHasCapableAccelerometer
+    return !_deviceHasCapableAccelerometer
         ? MultiProvider(
             providers: [
               ChangeNotifierProvider<UserProfileController>(
