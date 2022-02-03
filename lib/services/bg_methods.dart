b49c053c23e51278144e63a4e90495386409ed2c
hiruthicShaSS <predatorsha2002@gmail.com>
2022-02-03 12:44:02 +0530

    Using acceleromenter for activity detection in bg

--- lib/services/bg_methods.dart
+++ lib/services/bg_methods.dart
@@ -6,6 +6,7 @@ import 'package:buzz_ai/services/upload_watcher.dart';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_background_service/flutter_background_service.dart';
+import 'package:sensors_plus/sensors_plus.dart';
 
 void onIosBackground() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -37,4 +38,42 @@ void onStart() {
 
   activityRecognitionApp.init();
   startUploadWatcher();
+  watchSensors();
+}
+
+void watchSensors() {
+  const targetX = 0.018938301;
+  const targetY = -0.040060136;
+  const targetZ = 0.042773451;
+
+  double totalX = 0;
+  double totalY = 0;
+  double totalZ = 0;
+
+  int counter = 0;
+
+
+  userAccelerometerEvents.listen((UserAccelerometerEvent event) {
+    totalX += event.x;
+    totalY += event.y;
+    totalZ += event.z;
+
+    counter++;
+    
+    if (counter >= 50) {
+      double currentAvgX = totalX / counter;
+      double currentAvgY = totalY / counter;
+      double currentAvgZ = totalZ / counter;
+
+      double diffX = currentAvgX - targetX;
+      double diffY = currentAvgY - targetY;
+      double diffZ = currentAvgZ - targetZ;
+
+      print([diffX.abs(), diffY.abs(), diffZ.abs()]);
+
+      counter = 0;
+    }
+  });
+
+
 }
